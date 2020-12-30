e2f7908d72c685b572ca4597036364633cbec8bf
ouche <oyshengxu@163.com>
2020-12-30 23:02:49 +0800

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,13 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('sessions.create');
@@ -21,7 +28,8 @@ class SessionsController extends Controller
 
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password], isset($request->remember))) {
             session()->flash('success', '登录成功');
-            return redirect()->route('users.show', Auth::user());
+            $fallback = route('users.show', Auth::user());
+            return redirect()->intended($fallback);
         } else {
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
