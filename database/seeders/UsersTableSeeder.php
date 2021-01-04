22ed178f93423510555e1f9a6c1e89958b46a56a
ouche <oyshengxu@163.com>
2021-01-04 22:30:22 +0800

    管理员删除用户

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'ouyang';
         $user->email = 'ouyang@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
